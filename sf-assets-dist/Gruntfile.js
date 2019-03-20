c61405308a90e27943f715234185aa6084bfbca4
Eric De Felix <eric.defelix@leapdev.io>
2019-03-21 02:08:02 +1100

    recompile sites css'

--- sf-assets-dist/Gruntfile.js
+++ sf-assets-dist/Gruntfile.js
@@ -23,5 +23,5 @@ module.exports = function(grunt) {
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.registerTask('default', ['sass']);
 }
