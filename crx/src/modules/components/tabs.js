44267bcfae9dfd86c64a96751566c4dc93628334
Eric de Felix <eric.defelix@leapdev.io>
2019-08-12 17:14:45 +1000

    container shadows

--- crx/src/modules/components/tabs.js
+++ crx/src/modules/components/tabs.js
@@ -132,7 +132,6 @@ export default class Tabs {
         editFieldTemplateFxn = this.editFieldTemplate,
         tabLinkTemplateFxn = this.tabLinkTemplate,
         tabBodyTemplateFxn = this.tabBodyTemplate,
-        tabSectionTemplateFxn = this.accordionSectionTemplate,
         updateTabsListFxn = this.updateTabsList;
 
       function deleteTabItem() {
@@ -233,81 +232,3 @@ export default class Tabs {
     }
   }
 }
-
-//   updateDOM(HTMLObject) {
-//     try {
-//       HTMLObject.querySelectorAll('.sf-tab-content').forEach((tabContainer) => {
-//         const tabID = tabContainer.id.split('tab-')[1];
-//         UserInterfaceSortable({
-//           container: document.getElementById(`canvasSubContainer_${tabID}`),
-//           contentDraggableClass: `.canvasDraggableSub_${tabID}`
-//         });
-//       });
-//     } catch (error) {
-//       console.log(error);
-//       console.log('NO HTML Object to attached to');
-//     }
-//   }
-// }
-
-// _bindEvtEditTabs: function () {
-//   const snippetContainer = document.getElementById(this.getAttribute('data-target'));
-//   if (!this.classList.contains('canvas-btn-primary')) {
-//     this.textContent = 'Save';
-//     this.classList.add('canvas-btn-primary');
-//     editor.btnSave.disabled = true;
-//     editor.btnPreview.disabled = true;
-//     snippetContainer.querySelectorAll('.sf-tab-item-link').forEach((element, index) => {
-//       element.contentEditable = true;
-//       element.parentElement.classList.add('edit-mode');
-
-//       if (index == 0) {
-//         let t;
-//         element.focus();
-
-//         t = setTimeout(() => {
-//           document.execCommand('selectAll', false, null);
-//           clearTimeout(t);
-//         }, 50);
-//       }
-
-//       if (element.parentElement.parentElement.children.length > 2) {
-//         element.parentElement
-//           .insertAdjacentHTML('afterbegin', '<button type="button" class="canvas-btn canvas-btn-delete"><span>×</span></button>');
-
-//         element.parentElement.querySelector('.canvas-btn-delete').onclick = function () {
-//           const
-//             targetTabIdToBeDeleted = this.nextElementSibling.id.split('target_')[1],
-//             tabLength = this.parentElement.parentElement.children.length,
-//             prevSibling = this.parentElement.previousElementSibling;
-
-//           document.getElementById(targetTabIdToBeDeleted).remove();
-
-//           if (tabLength > 2) this.parentElement.remove();
-//           if (tabLength == 3) {
-//             document.querySelectorAll('.canvas-btn-delete').forEach(element => {
-//               element.remove();
-//             });
-//           }
-//           if (this.parentElement.classList.contains('active') &&
-//             this.parentElement.nextElementSibling == null) prevSibling.querySelector('.sf-tab-item-link').click();
-
-//           editor.updateData();
-//         };
-//       }
-//     });
-//   }
-//   else {
-//     this.textContent = 'Edit Tabs';
-//     this.classList.remove('canvas-btn-primary');
-//     editor.btnSave.disabled = false;
-//     editor.btnPreview.disabled = false;
-//     snippetContainer.querySelectorAll('.sf-tab-item-link').forEach((element, index) => {
-//       element.contentEditable = false;
-//       element.removeAttribute('contentEditable');
-//       element.parentElement.classList.remove('edit-mode');
-
-//       if (element.previousElementSibling !== null) element.previousElementSibling.remove();
-//     });
-//   }
-// }
