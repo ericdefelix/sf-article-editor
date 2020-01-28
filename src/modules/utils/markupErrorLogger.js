e93e5057605182a300b07e27937ddaca283febee
Eric De Felix <eric.defelix@leapdev.io>
2020-01-29 09:55:33 +1100

    fix: HTML Hinter

--- src/modules/utils/markupErrorLogger.js
+++ src/modules/utils/markupErrorLogger.js
@@ -10,41 +10,39 @@ export default function storeMarkupErrors(ih) {
     'attr-no-duplication': true,
     'attr-lowercase': true
   };
-  const header =
-    '<ol class="sf-list-bullet-circular"><li><div class="sf-well"><h5 class="sf-well-heading">Click to edit heading</h5>';
-  const ihLint = HTMLHint.verify(header, config);
-  console.log(ihLint);
+  
+  const ihLint = HTMLHint.verify(JSON.stringify(ih), config);
   
   // Debug Log to Local storage
-  // if (ihLint.length > 0) {
-  //   const localStorageArray = JSON.parse(localStorage.getItem('DebugLog')) || [];
-  //   let messageArr = [...localStorageArray];
-  //   const gmtErrotDate = new Date();
+  if (ihLint.length > 0) {
+    const localStorageArray = JSON.parse(localStorage.getItem('DebugLog')) || [];
+    let messageArr = [...localStorageArray];
+    const gmtErrotDate = new Date();
 
-  //   ihLint.forEach(({ message }) => {
-  //     const isRepeated =
-  //       typeof localStorageArray.find(i => i.message === message) === 'object' ||
-  //       typeof messageArr.find(i => i.message === message) === 'object';
+    ihLint.forEach(({ message }) => {
+      const isRepeated =
+        typeof localStorageArray.find(i => i.message === message) === 'object' ||
+        typeof messageArr.find(i => i.message === message) === 'object';
 
-  //     const newItem = {
-  //       // type,
-  //       date: gmtErrotDate.toLocaleString(),
-  //       message: message,
-  //       markup: ih
-  //     };
+      const newItem = {
+        // type,
+        date: gmtErrotDate.toLocaleString(),
+        message: message,
+        markup: ih
+      };
 
-  //     if(!isRepeated) {
-  //       messageArr = [
-  //         ...messageArr,
-  //         newItem
-  //       ];
-  //     } else {
-  //       messageArr = [
-  //         ...messageArr,
-  //       ];
-  //     }
-  //   });
+      if(!isRepeated) {
+        messageArr = [
+          ...messageArr,
+          newItem
+        ];
+      } else {
+        messageArr = [
+          ...messageArr,
+        ];
+      }
+    });
     
-  //   localStorage.setItem('DebugLog', JSON.stringify(messageArr));
-  // }
+    localStorage.setItem('DebugLog', JSON.stringify(messageArr));
+  }
 }
